50091dee0f010b885bcfb4580c9cc40becf0c9b2
lhphat08 <lehuuphat08@gmail.com>
2022-09-27 11:18:46 +0700

    Update k8s

--- canvas-lms/start.sh
+++ canvas-lms/start.sh
@@ -1,4 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-bundle exec rake brand_configs:generate_and_upload_all
-bundle exec puma -p 3000 -w 1 -e production
+set -e
+
+sed -i "s/EMAIL_DELIVERY_METHOD/${EMAIL_DELIVERY_METHOD-test}/" /opt/canvas/canvas-lms/config/outgoing_mail.yml
+sed -i "s/SMTP_ADDRESS/${SMTP_ADDRESS-localhost}/" /opt/canvas/canvas-lms/config/outgoing_mail.yml
+sed -i "s/SMTP_PORT/${SMTP_PORT-25}/" /opt/canvas/canvas-lms/config/outgoing_mail.yml
+sed -i "s/SMTP_USER/${SMTP_USER-}/" /opt/canvas/canvas-lms/config/outgoing_mail.yml
+sed -i "s/SMTP_PASS/${SMTP_PASS-}/" /opt/canvas/canvas-lms/config/outgoing_mail.yml
+
+exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
